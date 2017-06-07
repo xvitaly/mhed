a2ae67f72aeaa9f8a2c44c2b80f1785579e6b4f2
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-07 20:43:05 +0300

    Added build script.

---
+++ scripts/build_win.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+echo Starting build process using MSBUILD...
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\mhed.sln /m /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
+
+echo Changing directory to built version...
+cd "..\src\bin\Release"
+
+echo Signing binaries...
+"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A mhed.exe
+"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A ru/srcrepair.resources.dll
+
