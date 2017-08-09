001e0cb8b7baaa2206d6efcffecc653092e85661
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-09 20:23:23 +0300

    Updated build script.

--- scripts/build_win.cmd
+++ scripts/build_win.cmd
@@ -10,3 +10,6 @@ echo Signing binaries...
 "%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A mhed.exe
 "%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A ru/mhed.resources.dll
 
+echo Compiling Installer...
+"%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" srcrepair.iss
+
