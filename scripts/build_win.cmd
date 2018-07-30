371059e197f473eed38c4d9a7c1fbdf4a80a2811
Vitaly Zaitsev <vitaly@easycoding.org>
2018-07-30 19:08:42 +0200

    Added GPL preamble. Fixed build on latest releases.

--- scripts/build_win.cmd
+++ scripts/build_win.cmd
@@ -1,5 +1,25 @@
 @echo off
 
+rem Micro Hosts Editor (standalone application).
+rem 
+rem Copyright (c) 2011 - 2018 EasyCoding Team (ECTeam).
+rem Copyright (c) 2005 - 2018 EasyCoding Team.
+rem 
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem 
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTYrem without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem 
+rem You should have received a copy of the GNU General Public License
+rem along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+set GPGKEY=A989AAAA
+
 echo Starting build process using MSBUILD...
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\mhed.sln /m /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
 
@@ -7,12 +27,11 @@ echo Changing directory to built version...
 cd "..\src\bin\Release"
 
 echo Signing binaries...
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A mhed.exe
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A ru/mhed.resources.dll
+"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% mhed.exe
+"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% ru/mhed.resources.dll
 
-echo Converting README from Markdown to RTF...
-"%ProgramFiles(x86)%\Pandoc\pandoc.exe" -s README.md -o readme.rtf
+echo Fixing README file...
+"%ProgramFiles%\Git\usr\bin\sed.exe" -e '3,4d' -i README.md
 
 echo Compiling Installer...
 "%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" mhed.iss
-
