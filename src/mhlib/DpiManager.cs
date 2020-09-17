859a7ef0cf5d5462db9430c70290de52923e43d9
Vitaly Zaitsev <vitaly@easycoding.org>
2020-09-17 14:05:12 +0200

    Extracted Micro Hosts Editor core library.

---
+++ src/mhlib/DpiManager.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This file is a part of Micro Hosts Editor. For more information
+ * visit official site: https://www.easycoding.org/projects/mhed
+ *
+ * Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2020 EasyCoding Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace mhed.lib
+{
+    class DpiManager
+    {
+        /// <summary>
+        /// Correctly scale columns width in DataGridView container on high
+        /// pixel density displays.
+        /// </summary>
+        /// <param name="ScaleSource">DataGridView control name.</param>
+        /// <param name="ScaleFactor">Scale factor value.</param>
+        public static void ScaleColumnsInControl(DataGridView ScaleSource, SizeF ScaleFactor)
+        {
+            foreach (DataGridViewColumn Column in ScaleSource.Columns)
+            {
+                Column.Width = (int)Math.Round(Column.Width * ScaleFactor.Width);
+            }
+        }
+
+        /// <summary>
+        /// Compare two floating point numbers.
+        /// </summary>
+        /// <param name="First">First floating point number.</param>
+        /// <param name="Second">Second floating point number.</param>
+        /// <returns>Comparison result.</returns>
+        public static bool CompareFloats(float First, float Second)
+        {
+            return Math.Abs(First - Second) < 0.00001f;
+        }
+    }
+}
---
+++ src/mhlib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Micro Hosts Editor Core Library")]
+[assembly: AssemblyDescription("Micro Hosts Editor Core Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EasyCoding Team")]
+[assembly: AssemblyProduct("Micro Hosts Editor Core Library")]
+[assembly: AssemblyCopyright("(c) 2005-2020 EasyCoding Team. All rights reserved.")]
+[assembly: AssemblyTrademark("EasyCoding, EasyCoding Software Group and EasyCoding Team are trademarks of EasyCoding Team.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9b3cf304-8d8d-4cfe-9935-ceee67d1dcfd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.0.338")]
+[assembly: AssemblyFileVersion("0.9.0.338")]
