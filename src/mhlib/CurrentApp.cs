4e5a3b2a4dddad2358971498b223d99eb511a4dc
Vitaly Zaitsev <vitaly@easycoding.org>
2020-09-17 16:32:00 +0200

    Fixed a minor issue with unused semicolon.

--- src/mhlib/CurrentApp.cs
+++ src/mhlib/CurrentApp.cs
@@ -134,7 +134,7 @@ namespace mhed.lib
             FullAppPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
 
             // Getting full to application user directory...
-            AppUserDir = IsPortable ? Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), "portable") : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName); ;
+            AppUserDir = IsPortable ? Path.Combine(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location), "portable") : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName);
 
             // Checking if user directory exists. If not - creating it...
             if (!Directory.Exists(AppUserDir))
