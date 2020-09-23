xvitaly/mhed

commit 39dfd623f86f4d2638b1f84c30b927d63a6a02ab

Changed the List to the BindingList for the Contents public property.

src/mhlib/HostsFileManager.cs
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -44,7 +44,7 @@ namespace mhed.lib
         /// <summary>
         /// Get or set Hosts file contents.
         /// </summary>
-        public List<HostsFileEntry> Contents { get; private set; }
+        public BindingList<HostsFileEntry> Contents;
 
         /// <summary>
         /// Clear Hosts file data object.
@@ -138,7 +138,7 @@ namespace mhed.lib
         {
             FilePath = FileManager.GetHostsFileFullPath(OS);
             Platform = OS;
-            Contents = new List<HostsFileEntry>();
+            Contents = new BindingList<HostsFileEntry>();
             if (AutoLoad) Load();
         }
     }

commit 08d716273bbf262eb756118c15f872856464532a

Fixed Contents property of the HostsFileManager class.
Regression introduced by 39dfd623f86f4d2638b1f84c30b927d63a6a02ab.

src/mhlib/HostsFileManager.cs
@@ -44,7 +44,7 @@ namespace mhed.lib
         /// <summary>
         /// Get or set Hosts file contents.
         /// </summary>
-        public BindingList<HostsFileEntry> Contents;
+        public BindingList<HostsFileEntry> Contents { get; private set; }
 
         /// <summary>
         /// Clear Hosts file data object.