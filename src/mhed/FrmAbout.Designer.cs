c110b9893a19d2fe8ae86b0ea9ce3f848802bd28
Vitaly Zaitsev <vitaly@easycoding.org>
2020-09-29 18:59:39 +0200

    Added About form.
    Closes #59.

---
+++ src/mhed/FrmAbout.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace mhed.gui
+{
+    partial class FrmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAbout));
+            this.AHE_AppIcon = new System.Windows.Forms.PictureBox();
+            this.AHE_AppName = new System.Windows.Forms.Label();
+            this.AHE_Version = new System.Windows.Forms.Label();
+            this.AHE_CompanyName = new System.Windows.Forms.Label();
+            this.AHE_Description = new System.Windows.Forms.TextBox();
+            this.AHE_License = new System.Windows.Forms.Label();
+            this.AHE_Copyright = new System.Windows.Forms.Label();
+            this.AHE_Okay = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.AHE_AppIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AHE_AppIcon
+            // 
+            this.AHE_AppIcon.Image = global::mhed.gui.Properties.Resources.AppIcon;
+            resources.ApplyResources(this.AHE_AppIcon, "AHE_AppIcon");
+            this.AHE_AppIcon.Name = "AHE_AppIcon";
+            this.AHE_AppIcon.TabStop = false;
+            // 
+            // AHE_AppName
+            // 
+            resources.ApplyResources(this.AHE_AppName, "AHE_AppName");
+            this.AHE_AppName.Name = "AHE_AppName";
+            // 
+            // AHE_Version
+            // 
+            resources.ApplyResources(this.AHE_Version, "AHE_Version");
+            this.AHE_Version.Name = "AHE_Version";
+            // 
+            // AHE_CompanyName
+            // 
+            resources.ApplyResources(this.AHE_CompanyName, "AHE_CompanyName");
+            this.AHE_CompanyName.Name = "AHE_CompanyName";
+            // 
+            // AHE_Description
+            // 
+            resources.ApplyResources(this.AHE_Description, "AHE_Description");
+            this.AHE_Description.Name = "AHE_Description";
+            this.AHE_Description.ReadOnly = true;
+            this.AHE_Description.TabStop = false;
+            // 
+            // AHE_License
+            // 
+            resources.ApplyResources(this.AHE_License, "AHE_License");
+            this.AHE_License.Name = "AHE_License";
+            // 
+            // AHE_Copyright
+            // 
+            resources.ApplyResources(this.AHE_Copyright, "AHE_Copyright");
+            this.AHE_Copyright.Name = "AHE_Copyright";
+            // 
+            // AHE_Okay
+            // 
+            this.AHE_Okay.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.AHE_Okay, "AHE_Okay");
+            this.AHE_Okay.Name = "AHE_Okay";
+            this.AHE_Okay.UseVisualStyleBackColor = true;
+            this.AHE_Okay.Click += new System.EventHandler(this.AHE_Okay_Click);
+            // 
+            // FrmAbout
+            // 
+            this.AcceptButton = this.AHE_Okay;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.AHE_Okay;
+            this.Controls.Add(this.AHE_Okay);
+            this.Controls.Add(this.AHE_Copyright);
+            this.Controls.Add(this.AHE_License);
+            this.Controls.Add(this.AHE_Description);
+            this.Controls.Add(this.AHE_CompanyName);
+            this.Controls.Add(this.AHE_Version);
+            this.Controls.Add(this.AHE_AppName);
+            this.Controls.Add(this.AHE_AppIcon);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmAbout";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Load += new System.EventHandler(this.FrmAbout_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.AHE_AppIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AHE_AppIcon;
+        private System.Windows.Forms.Label AHE_AppName;
+        private System.Windows.Forms.Label AHE_Version;
+        private System.Windows.Forms.Label AHE_CompanyName;
+        private System.Windows.Forms.TextBox AHE_Description;
+        private System.Windows.Forms.Label AHE_License;
+        private System.Windows.Forms.Label AHE_Copyright;
+        private System.Windows.Forms.Button AHE_Okay;
+
+    }
+}
---
+++ src/mhed/FrmAbout.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * This file is a part of Micro Hosts Editor. For more information
+ * visit official site: https://www.easycoding.org/projects/mhed
+ *
+ * Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2020 EasyCoding Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using mhed.lib;
+using System;
+using System.Windows.Forms;
+
+namespace mhed.gui
+{
+    /// <summary>
+    /// "About" form class.
+    /// </summary>
+    partial class FrmAbout : Form
+    {
+        /// <summary>
+        /// FrmAbout class constructor.
+        /// </summary>
+        public FrmAbout()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// "OK" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void AHE_Okay_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// "Form create" event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void FrmAbout_Load(object sender, EventArgs e)
+        {
+            // Adding information about product version and copyrights...
+            Text = String.Format(Text, CurrentApp.AppProduct);
+            AHE_AppName.Text = CurrentApp.AppProduct;
+            #if DEBUG
+            AHE_AppName.Text += " DEBUG";
+            #endif
+            AHE_Version.Text = String.Format("Version: {0}", CurrentApp.AppVersion);
+            AHE_Copyright.Text = CurrentApp.AppCopyright;
+            AHE_CompanyName.Text = CurrentApp.AppCompany;
+        }
+    }
+}
