110b6bc323c5be3e7bc77addfd974cf25a5ee9bb
Vitaly Zaitsev <vitaly@easycoding.org>
2020-10-08 15:56:25 +0200

    Added a new class ReturnCodes.

---
+++ src/mhlib/ReturnCodes.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * This file is a part of Micro Hosts Editor. For more information
+ * visit official site: https://www.easycoding.org/projects/mhed
+ *
+ * Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2020 EasyCoding Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace mhed.lib
+{
+    /// <summary>
+    /// Class for working with application return codes.
+    /// </summary>
+    public static class ReturnCodes
+    {
+        /// <summary>
+        /// Successful exit without any errors.
+        /// </summary>
+        public const int Success = 0;
+
+        /// <summary>
+        /// Application is already running.
+        /// </summary>
+        public const int AppAlreadyRunning = 7;
+
+        /// <summary>
+        /// Core library version missmatch.
+        /// </summary>
+        public const int CoreLibVersionMissmatch = 8;
+    }
+}
