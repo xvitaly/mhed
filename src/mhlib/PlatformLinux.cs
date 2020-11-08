e066850a22e4aae277a683f21b37da41b38baae1
Vitaly Zaitsev <vitaly@easycoding.org>
2020-11-08 18:19:44 +0100

    Added PlatformLinux public class.
    Closes #81.

---
+++ src/mhlib/PlatformLinux.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * This file is a part of Micro Hosts Editor. For more information
+ * visit official site: https://www.easycoding.org/projects/mhed
+ *
+ * Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2020 EasyCoding Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Permissions;
+
+namespace mhed.lib
+{
+    public class PlatformLinux : CurrentPlatform
+    {
+        /// <summary>
+        /// Open the specified text file in default (or overrided in application's
+        /// settings (only on Windows platform)) text editor.
+        /// </summary>
+        /// <param name="FileName">Full path to text file.</param>
+        /// <param name="EditorBin">External text editor (Windows only).</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public override void OpenTextEditor(string FileName, string EditorBin)
+        {
+            Process.Start(Properties.Resources.AppOpenHandlerLin, AddQuotesToPath(FileName));
+        }
+
+        /// <summary>
+        /// Show the specified file in default file manager.
+        /// </summary>
+        /// <param name="FileName">Full path to file.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public override void OpenExplorer(string FileName)
+        {
+            Process.Start(Properties.Resources.AppOpenHandlerLin, String.Format("\"{0}\"", Path.GetDirectoryName(FileName)));
+        }
+
+        /// <summary>
+        /// Start the required application from administrator.
+        /// </summary>
+        /// <param name="FileName">Full path to the executable.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public override int StartElevatedProcess(string FileName)
+        {
+            // Setting advanced properties...
+            ProcessStartInfo ST = new ProcessStartInfo()
+            {
+                FileName = FileName,
+                Verb = "pkexec",
+                WindowStyle = ProcessWindowStyle.Normal,
+                UseShellExecute = true
+            };
+
+            // Starting process...
+            Process NewProcess = Process.Start(ST);
+
+            // Returning PID of created process...
+            return NewProcess.Id;
+        }
+
+        /// <summary>
+        /// Restart current application with admin user rights.
+        /// </summary>
+        /// <param name="OS">Operating system type.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public override void RestartApplicationAsAdmin()
+        {
+            StartElevatedProcess(Path.GetFileNameWithoutExtension(CurrentApp.AssemblyLocation));
+            Environment.Exit(ReturnCodes.Success);
+        }
+
+        /// <summary>
+        /// Get current operating system ID.
+        /// </summary>
+        public override OSType OS => OSType.Linux;
+    }
+}
