95787402d51b1e9f00de8e9dbc0fd2df22938830
Vitaly Zaitsev <vitaly@easycoding.org>
2022-12-05 15:07:17 +0100

    Added FormShowDownloader() and FormShowUpdater() public static methods to GuiHelpers class.

--- src/mhed/GuiHelpers.cs
+++ src/mhed/GuiHelpers.cs
@@ -12,6 +12,20 @@ namespace mhed.gui
     public static class GuiHelpers
     {
         /// <summary>
+        /// Open downloader window and download a specified file
+        /// to a specified location.
+        /// </summary>
+        /// <param name="URI">Download URL.</param>
+        /// <param name="FileName">Full path to destination file.</param>
+        public static void FormShowDownloader(string URI, string FileName)
+        {
+            using (FrmDnWrk DnW = new FrmDnWrk(URI, FileName))
+            {
+                DnW.ShowDialog();
+            }
+        }
+
+        /// <summary>
         /// Open "About applicaion" form window.
         /// </summary>
         public static void FormShowAboutApp()
@@ -19,6 +33,20 @@ namespace mhed.gui
             using (FrmAbout AboutFrm = new FrmAbout())
             {
                 AboutFrm.ShowDialog();
+            }
+        }
+
+        /// <summary>
+        /// Open updater window.
+        /// </summary>
+        /// <param name="UserAgent">User-Agent header for outgoing HTTP queries.</param>
+        /// <param name="FullAppPath">App's installation directory.</param>
+        /// <param name="AppUpdateDir">App's local updates directory.</param>
+        public static void FormShowUpdater(string UserAgent, string FullAppPath, string AppUpdateDir)
+        {
+            using (FrmUpdate UpdFrm = new FrmUpdate(UserAgent, FullAppPath, AppUpdateDir))
+            {
+                UpdFrm.ShowDialog();
             }
         }
 
