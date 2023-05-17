02076de2f649a385e8ebb3a2da18b0fa6291227a
Vitaly Zaitsev <vitaly@easycoding.org>
2023-05-17 13:31:47 +0200

    Added a new class SystemResolver.
    Closes #163.

---
+++ src/mhlib/SystemResolver.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * SPDX-FileCopyrightText: 2011-2023 EasyCoding Team
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+using System;
+using System.Threading.Tasks;
+
+namespace mhed.lib
+{
+    /// <summary>
+    /// Class for working with system DNS resolver.
+    /// </summary>
+    public class SystemResolver : CurrentResolver
+    {
+        /// <summary>
+        /// Resolve the specified hostname using system resolver
+        /// and return the associated IP-address.
+        /// </summary>
+        /// <param name="Hostname">Hostname to be resolved.</param>
+        /// <returns>Associated IP-address.</returns>
+        public override Task<string> Resolve(string Hostname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
