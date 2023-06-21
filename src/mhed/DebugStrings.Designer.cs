ca238dba6623fa8c87af2e5a0c176e1dcf9e5d81
Vitaly Zaitsev <vitaly@easycoding.org>
2023-06-21 17:41:21 +0200

    Updated AppDbgModelViewColumnIndexOutOfRange debug string text.

--- src/mhed/DebugStrings.Designer.cs
+++ src/mhed/DebugStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace mhed.gui {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class DebugStrings {
@@ -304,7 +304,7 @@ namespace mhed.gui {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Invalid column index detected. Unable to validate..
+        ///   Looks up a localized string similar to Invalid column index detected. Unable to perform the requested action!.
         /// </summary>
         internal static string AppDbgModelViewColumnIndexOutOfRange {
             get {
