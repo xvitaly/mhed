b8f46c55d560ca7e45725636729c6ba247191ef2
Vitaly <vitaly@easycoding.org>
2023-10-13 13:14:41 +0200

    Added a new class HostsFileEntryParser.

---
+++ src/mhlib/HostsFileEntryParser.cs
@@ -0,0 +1,18 @@
+﻿/**
+ * SPDX-FileCopyrightText: 2011-2023 EasyCoding Team
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhed.lib
+{
+    public sealed class HostsFileEntryParser
+    {
+    }
+}
