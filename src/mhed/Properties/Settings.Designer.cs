4c7231db0dca6c061e32aa34df9d3ffd011e629b
Vitaly <vitaly@easycoding.org>
2023-10-24 05:18:38 +0200

    Added a new FormMaximized setting.

--- src/mhed/Properties/Settings.Designer.cs
+++ src/mhed/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace mhed.gui.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -137,6 +137,18 @@ namespace mhed.gui.Properties {
             }
             set {
                 this["LastCleanupTime"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool FormMaximized {
+            get {
+                return ((bool)(this["FormMaximized"]));
+            }
+            set {
+                this["FormMaximized"] = value;
             }
         }
     }
