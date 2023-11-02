11508f6289134953d7c3cf689ef7b5d59ac5cb2f
Vitaly <vitaly@easycoding.org>
2023-11-02 10:51:50 +0100

    Added a new resource string NewEntrySingle.

--- src/mhlib/Properties/Resources.Designer.cs
+++ src/mhlib/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace mhed.lib.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -149,6 +149,15 @@ namespace mhed.lib.Properties {
         internal static string MutexRegular {
             get {
                 return ResourceManager.GetString("MutexRegular", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} {1}.
+        /// </summary>
+        internal static string NewEntrySingle {
+            get {
+                return ResourceManager.GetString("NewEntrySingle", resourceCulture);
             }
         }
         
